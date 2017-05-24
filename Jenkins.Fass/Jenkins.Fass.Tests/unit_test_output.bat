a824b9a19b39dd4581d8ff58c869dfd9744a94b2
t-roberts <troberts@claritycon.com>
2017-05-24 12:23:19 -0500

    fixing

--- Jenkins.Fass/Jenkins.Fass.Tests/unit_test_output.bat
+++ Jenkins.Fass/Jenkins.Fass.Tests/unit_test_output.bat
@@ -57,7 +57,7 @@ echo xdir: %xdir%
 set testAssemblies=
 for /f "tokens=*" %%a in ('dir /b /s /a:d "%root%\*.Tests"') do (
   :: copy the execution library into each test library output folder
-  copy "%xdir%\xunit.execution.desktop.dll" "%%a\bin\%configuration%\" >NUL
+  copy "C:\Program Files (x86)\Jenkins\workspace\Fass-Freestyle\Jenkins.Fass\Jenkins.Fass.Tests\bin\debug\xunit\xunit.execution.desktop.dll" "%%a\bin\%configuration%\" >NUL
   :: add this assembly to the list of assemblies (delayed expansion)
   set testAssembly=^"%%a\bin\%configuration%\%%~nxa.dll^"
   if [!testAssemblies!]==[] (
