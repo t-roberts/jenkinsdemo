ebc98a80bd04c4cc5b5996995a2040ab010e3b50
t-roberts <troberts@claritycon.com>
2017-05-24 12:07:12 -0500

    removing

--- Jenkins.Fass/Jenkins.Fass.Tests/unit_test_output.bat
+++ Jenkins.Fass/Jenkins.Fass.Tests/unit_test_output.bat
@@ -57,7 +57,7 @@ echo xdir: %xdir%
 set testAssemblies=
 for /f "tokens=*" %%a in ('dir /b /s /a:d "%root%\*.Tests"') do (
   :: copy the execution library into each test library output folder
-  copy "%xdir%\xunit.execution.desktop.dll" "%%a\bin\%configuration%\" >NUL
+  copy "%xdir%xunit.execution.desktop.dll" "%%a\bin\%configuration%\" >NUL
 
   :: add this assembly to the list of assemblies (delayed expansion)
   set testAssembly=^"%%a\bin\%configuration%\%%~nxa.dll^"
